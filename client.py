dd656f51a7400808d8e54c5d56796652060806a7
BMichaud7 <brendanmichaud7@gmail.com>
2021-03-29 12:33:20 -0400

    Done

--- client.py
+++ client.py
@@ -4,29 +4,41 @@ import socket
 import sys
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
+    #gets args from user to use in the program
     host = sys.argv[1]
     port = int(sys.argv[2])
     request_type = sys.argv[3]
     File = sys.argv[4]
-    print(sys.argv)
+    # print(sys.argv)
+    #gets url request string ready to send info to the server. Consist of request type and host ip and port.
     url_string =  request_type + " /" +File + " HTTP/1.1\r\nHost: "+host+"\r\nAccept: text/html\r\nConnection: close\r\n\r\n"
+    #connect to server
     s.connect((host , port))
+    #send url_string to server
     s.sendall(url_string.encode())
+
+    # print("REQ",request_type)
+    #what request type is it? 
+    #if get re wait for the server to respond.
     if request_type == "GET":
         while True:
             data = s.recv(1024)
             if not data:
                 break
-            print(data.decode())
+            # print(data.decode())
+        s.close()
+    # if put open file and sent it to the server.
     elif request_type == "PUT":
         with open(File, "r") as f:
+            # print("READING NOW",f)
             for l in f.readlines():
                 print(l)
                 s.sendall(str.encode(""+l+""))
                 l = f.read(1024)
         f.close()
+        #send file to server
         data = s.recv(1024)
+        #print responce
         print(data.decode())
         s.close()
         print('connection closed')
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 import sys
 
-
+#multi_server.py ony difference is everyting here is in a function and everytime someone connects we make a new thread.
 # Define socket host and port
 SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 8000
+SERVER_PORT = int(sys.argv[1])
 
 # Create socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,9 +18,14 @@ while True:
     client_connection, client_address = server_socket.accept()
     # Get the client request
     try:
+        # print("IN TRY")
+        #get request which is url_request string
         request = client_connection.recv(1024).decode()
+        #get the file name that is being requested or going to be written.
         file_request = request.split()[1]
+        #if no file name default to index.html
         file_request = "index.html" if file_request == "/" else file_request[1:]
+        # if get request send file to client and send responce
         if request.split()[0] == "GET":
             with open(file_request, "r") as f:
                 outputdata = f.read()
@@ -32,16 +37,19 @@ while True:
             client_connection.send(response.encode())
             print("response sent")
 
-    
+            #swend data and file to client
             for i in range(0, len(outputdata)):
                 client_connection.send(outputdata[i].encode())
+
             client_connection.send("\r\n".encode())
             client_connection.close()
+        #if put  open file snet by client and send responce if file was created.
         elif request.split()[0] == "PUT":
             filename = request.split()[1].replace("/", "")
             print("SAVING: ",filename)
             with open(filename, 'wb') as f:
                 client_connection.sendall(str.encode("200 OK File Created"))
+                #saving file.
                 while True:
                     print('receiving data...')
                     data = client_connection.recv(1024)
@@ -51,7 +59,6 @@ while True:
                     f.write(data) 
                 f.close()
             
-
     except IOError:
         # Send response message for file not found
         response = ("HTTP/1.1 404 Not Found\n"
